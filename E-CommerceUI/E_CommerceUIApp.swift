0aeb4312c8b1abcdeee549b1fff60954165d9c29
Kanishk Vijaywargiya <secretcodersociety@gmail.com>
2021-08-31 14:29:34 +0530

    Initial Commit

---
+++ E-CommerceUI/E_CommerceUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  E_CommerceUIApp.swift
+//  E-CommerceUI
+//
+//  Created by KANISHK VIJAYWARGIYA on 31/08/21.
+//
+
+import SwiftUI
+
+@main
+struct E_CommerceUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
